ace258a261cb6386348187a9819d187a1fca98f8
chuiyan <xzcxin061@163.com>
2023-05-09 11:05:49 +0800

    更换初始投资组合

--- app/controller/Stock.php
+++ app/controller/Stock.php
@@ -3,7 +3,7 @@
  * @Author: chuiyan 
  * @Date: 2022-05-23 15:29:06
  * @LastEditors: chuiyan xzcxin061@163.com
- * @LastEditTime: 2023-05-08 19:00:12
+ * @LastEditTime: 2023-05-09 11:03:18
  * @FilePath: /woodsmoke/app/controller/Stock.php
  * @Description: 
  * 
@@ -16,35 +16,65 @@ use app\model\StockDaily;
 
 class Stock
 {
-    // 选择投资组合，数组和字符串二选一
-    protected $stockCodeArr = ['002228', '002345', '600016', '600668', '601186', '601336'];
-    protected $stockCodeStr = '002228,002345,600016,600668,601186,601336';
+    // 选择投资组合1，数组和字符串二选一
+    // protected $stockCodeArr = ['002228', '002345', '600016', '600668', '601186', '601336'];
+    // protected $stockCodeStr = '002228,002345,600016,600668,601186,601336';
     // 投资期限-最大回撤
-    protected $startTime = '210720';
-    protected $stopTime = '230320';
+    // protected $startTime = '210720';
+    // protected $stopTime = '230320';
+    // 原始投资组合，最小为二维数组。参数：日期，股数，均价：[code, date, shares, value]
+    // protected $handleBuyArr = [
+    //     '002228' => ['002228', '20210720', 5000, 3.564], 
+    //     '002345' => ['002345', '20210720', 3000, 5.85],
+    //     '600016' => ['600016', '20210720', 2000, 4.17],
+    //     '600668' => ['600668', '20210720', 1000, 11.14],
+    //     '601186' => ['601186', '20210720', 5000, 7.29],
+    //     '601336' => ['601336', '20210720', 1500, 44.48]
+    // ];  
+    // 买入记录，最小为三维数组。参数：日期，股数，均价：[code, date, shares, value]
+    // protected $buyArr = [
+    //     '002228' => [['002228', '20220622', 4000, 3.41]],
+    // ];
+// 卖出记录，最小为三维数组。参数：日期，股数，均价：[code, date, shares, value]
+// protected $saleArr = [
+//         '002228' => [['002228', '20221011', -6000, 3.10]],
+//         '002345' => [['002345', '20230213', -1500, 6.55]]
+//     ];
+    
 
+    // 股票数量少，手动处理比较快，写代码查询第一天的收盘价再拼数组也行。
+    // 选择投资组合2，数组和字符串二选一
+    protected $stockCodeArr = ['601319', '601688', '600623', '600810', '600475', '600585', '600104', '600997', '000725', '600028', '600398', '600308', '600782', '600000'];  
+    protected $stockCodeStr = '601319,601688,600623,600810,600475,600585,600104,600997,000725,600028,600398,600308,600782,600000';
+    protected $startTime = '220602';
+    protected $stopTime = '230505';
+    // 原始投资组合2，最小为二维数组。参数：日期，股数，均价：[code, date, shares, value]
+    // 每只股票投资金额30000元，按100股为单位，向下取整。
+    protected $handleBuyArr = [
+       '000725' => ['000725', '20220602', 7900, 3.76], 
+       '600000' => ['600000', '20220602', 3800, 7.89],
+       '600028' => ['600028', '20220602', 6700, 4.42],
+       '600104' => ['600104', '20220602', 1600, 17.75],
+       '600308' => ['600308', '20220602', 5100, 5.88],
+       '600398' => ['600398', '20220602', 5700, 5.25],
+       '600475' => ['600475', '20220602', 3600, 8.33],    
+       '600585' => ['600585', '20220602', 800, 36.22], 
+       '600623' => ['600623', '20220602', 3400, 8.61], 
+       '600782' => ['600782', '20220602', 5200, 5.75], 
+       '600810' => ['600810', '20220602', 3200, 9.26], 
+       '600997' => ['600997', '20220602', 4000, 7.42], 
+       '601319' => ['601319', '20220602', 6500, 4.61], 
+       '601688' => ['601688', '20220602', 2200, 13.32], 
+    ];    
+    protected $buyArr = [];
+    protected $saleArr = [];    
+
+    // 公用变量
     protected $resultArr = [];
     protected $finalArr = [];
     protected $handleFinalArr = [];
 
-    // 601319
-    // 601688
-    // 600623
-    // 600810
-    // 600475
-    // 600585
-    // 600104
-    // 600997
-    // 000725
-    // 600028
-    // 600398
-    // 600308
-    // 600782
-    // 600000
-
-
-
-    // 原始投资组合，参数：日期，股数，价格：[code, date, shares, value]
+    // 已弃用：原始投资组合，参数：日期，股数，价格：[code, date, shares, value]
     // protected $originBuyArr = [
     //                                 ['002228', '20210720', 2000, 3.60], 
     //                                 ['002228', '20210720', 3000, 3.54], 
@@ -54,25 +84,7 @@ class Stock
     //                                 ['601186', '20210720', 5000, 7.29],
     //                                 ['601336', '20210720', 1500, 44.48]
     //                             ]; 
-    // 原始投资组合，最小为二维数组。参数：日期，股数，均价：[code, date, shares, value]
-    protected $handleBuyArr = [
-                                '002228' => ['002228', '20210720', 5000, 3.564], 
-                                '002345' => ['002345', '20210720', 3000, 5.85],
-                                '600016' => ['600016', '20210720', 2000, 4.17],
-                                '600668' => ['600668', '20210720', 1000, 11.14],
-                                '601186' => ['601186', '20210720', 5000, 7.29],
-                                '601336' => ['601336', '20210720', 1500, 44.48]
-    ];     
-    // 买入记录，最小为三维数组。参数：日期，股数，均价：[code, date, shares, value]
-    protected $buyArr = [
-                            '002228' => [['002228', '20220622', 4000, 3.41]],
-                        ];
-    // 卖出记录，最小为三维数组。参数：日期，股数，均价：[code, date, shares, value]
-    protected $saleArr = [
-                            '002228' => [['002228', '20221011', -6000, 3.10]],
-                            '002345' => [['002345', '20230213', -1500, 6.55]]
-                        ];
-    
+
     /**
      * @Func 股票投资组合最大回撤率：在选定周期内任一历史时点往后推，产品净值走到最低点时的收益率回撤幅度的最大值。
      * @Describe 导出图表和数据，也可能会拆分该函数
@@ -96,10 +108,10 @@ class Stock
                 $this->resultArr[$val->stockcode]['x'][$val->date] = $val->date;
                 // y轴：收盘价
                 $this->resultArr[$val->stockcode]['y'][$val->date] = $val->close;
-
+                
                 // 计算每天的持有股数
                 $this->resultArr[$val->stockcode]['shares'][$val->date] = $this->handleBuyArr[$val->stockcode][2];
-                // 处理买入数据，2021年7月20日-2023年3月20，每天
+                // 处理买入数据，每天
                 if (!empty(array_filter($this->buyArr)) && !empty($this->buyArr[$val->stockcode])) {
                     foreach ($this->buyArr[$val->stockcode] as $k => $v) {
                         if ($v[1] <= $val->date) {
@@ -109,7 +121,7 @@ class Stock
                         }
                     }
                 }
-                // 处理卖出数据，2021年7月20日-2023年3月20，每天
+                // 处理卖出数据，每天
                 if (!empty(array_filter($this->saleArr)) && !empty($this->saleArr[$val->stockcode])) {
                     foreach ($this->saleArr[$val->stockcode] as $k => $v) {
                         if ($v[1] <= $val->date) {
@@ -135,10 +147,10 @@ class Stock
                 foreach ($value['x'] as $k => $v) {
                     // 设置均价每日默认值
                     $this->resultArr[$key]['junjia'][$v] = $originPriceAverage;
-                    // 收盘额，2021年7月20日-2023年3月20，按每天持有股数统计。
+                    // 收盘额，按每天持有股数统计。
                     $value['shoupane'][$v] = $value['shares'][$v] * $value['y'][$v];
                     // 计算均价,需要判断股票code存在
-                    // 处理买入数据，2021年7月20日-2023年3月20，每天
+                    // 处理买入数据，每天
                     if (!empty(array_filter($this->buyArr)) && !empty($this->buyArr[$key])) {
                         foreach ($this->buyArr[$key] as $val) {
                             if ($val[1] <= $v) {
@@ -151,15 +163,15 @@ class Stock
                             }
                         }
                     }
-                    // 处理卖出数据，2021年7月20日-2023年3月20，每天
+                    // 处理卖出数据，每天
                     if (!empty(array_filter($this->saleArr)) && !empty($this->saleArr[$key])) {
                         foreach ($this->saleArr[$key] as $val) {
                             if ($val[1] <= $v) {
                                 $this->resultArr[$key]['junjia'][$v] = round(
-                                                                            ($this->resultArr[$key]['junjia'][$v] * $value['junjia'][$v] + $val[3] * $val[2]) // 持有价值
-                                                                            / 
-                                                                            ($value['junjia'][$v] + $val[2]) // 持有股数
-                                                                       , 2
+                                        ($this->resultArr[$key]['junjia'][$v] * $value['junjia'][$v] + $val[3] * $val[2]) // 持有价值
+                                        / 
+                                        ($value['junjia'][$v] + $val[2]) // 持有股数
+                                    , 2
                                 );
                             }
                         }
@@ -173,14 +185,14 @@ class Stock
         // 分析思路
         // 
         if (!empty(array_filter($this->resultArr))) {
-            $orginAmountOuter = 0.00;
-            $shoupaneOuter = 0.00;
-            $shouyieOuter = 0.00;
-            $shouyilvOuter = 0.0000;            
+            // $orginAmountOuter = 0.00;
+            // $shoupaneOuter = 0.00;
+            // $shouyieOuter = 0.00;
+            // $shouyilvOuter = 0.0000;            
             foreach ($this->resultArr as $key => &$value) {
                 $yingkuieInner = 0.00;
-                $shouyieInner = 0.00;
-                $orginAmountInner = 0.00;
+                // $shouyieInner = 0.00;
+                // $orginAmountInner = 0.00;
                 foreach ($value['x'] as $k => $v) {
                     // 每天对应的原始投资额 = 每天的均价 * 每天的持有股数
                     $this->resultArr[$key]['orginAmount'][$v] = $value['junjia'][$v] * $value['shares'][$v];
@@ -198,9 +210,9 @@ class Stock
                             $this->resultArr[$key]['yingkuie'][$v] = $yingkuieInner;
                         }
                     }
-
                     // 更新收益额
                     $this->resultArr[$key]['shouyie'][$v] = $this->resultArr[$key]['shouyie'][$v] + $yingkuieInner;
+
                     // 每天的收益率 = 每天的收益额 / 每天的原始投资额
                     $this->finalArr[$v][$key]['shouyie'] = $this->resultArr[$key]['shouyie'][$v];
                     $this->finalArr[$v][$key]['orginAmount'] = $this->resultArr[$key]['orginAmount'][$v];
@@ -221,6 +233,7 @@ class Stock
                     $shouyilv = round($shouyie / $orginAmount, 4);
                 }
                 $this->handleFinalArr[$key]['shouyilv'] = $shouyilv;
+                $this->handleFinalArr[$key]['shouyie'] = $shouyie;
             }
         }
 
